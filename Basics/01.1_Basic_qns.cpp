10e0d9b0253fa75c8bd4c80b091d220487dbd484
jainkavya <jkavya85@gmail.com>
2025-04-04 01:43:09 +0530

    006

--- Basics/01.1_Basic_qns.cpp
+++ Basics/01.1_Basic_qns.cpp
@@ -1,11 +1,24 @@
 // Write a program to take an integer as input and print its square.
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main () {
-    int num ;
-    cout << " Enter the number to be squared : " ;
-    cin >> num ;
-    cout << " The square of " << num << " is " << num * num << endl;
-}
+// int main () {
+//     int num ;
+//     cout << " Enter the number to be squared : " ;
+//     cin >> num ;
+//     cout << " The square of " << num << " is " << (num * num) << endl;
+// }
+
+// Write a program to take two floating-point numbers as input and print their sum with 3 decimal places
+// int main () {
+//     double num1, num2;
+
+//     cout << " Enter 2 numbers : " << endl;
+//     cin >> num1 >> num2 ;
+//     cout << " Sum : " << fixed << setprecision(3) << num1 + num2 << endl;
+//     return 0;
+// }
+
+//
