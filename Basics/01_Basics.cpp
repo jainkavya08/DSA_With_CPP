c2a0c6408c11262f946767a1f46812b8bfa83352
jainkavya <jkavya85@gmail.com>
2025-04-04 00:59:29 +0530

    001

---
+++ Basics/01_Basics.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <iomanip>  
+using namespace std;
+
+/**************** Cout **********************/
+// int main () {
+//     cout << "Hello world " << endl;     // Prints a text
+//     cout << " The number is : " << 44 << endl;      // Prints a number 
+//     cout << " The sum of 5 and 3 is: " << 5 + 3 << endl;       // Prints and Expression
+
+//     return 0;
+// }
+
+
+
