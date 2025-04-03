b9a462f938fd5063ee67175f26e48fdafd5c85f5
jainkavya <jkavya85@gmail.com>
2025-04-04 01:29:05 +0530

    005

---
+++ Basics/01.1_Basic_qns.cpp
@@ -0,0 +1,11 @@
+// Write a program to take an integer as input and print its square.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int num ;
+    cout << " Enter the number to be squared : " ;
+    cin >> num ;
+    cout << " The square of " << num << " is " << num * num << endl;
+}
